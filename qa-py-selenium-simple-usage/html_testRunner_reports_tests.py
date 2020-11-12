3a40876095389f22ab70c86f30b75eb1ca514c93
unknown <valber.motorsport@gmail.com>
2020-11-12 21:10:02 +0200

    adding the html_testRunner_reports_tests.py file

---
+++ qa-py-selenium-simple-usage/html_testRunner_reports_tests.py
@@ -0,0 +1,37 @@
+# Installing 'html test runner' package: 'pip install html-testRunner'
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+# Importing the package:
+import HtmlTestRunner
+
+
+class LoginTest(unittest.TestCase):
+
+    @classmethod
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(10)
+
+    def test_01_searching_in_goolge(self):
+        self.driver.get("https://www.google.com")
+        assert "Google" in self.driver.title
+
+        elem = self.driver.find_element_by_name("q")
+        elem.clear()
+        elem.send_keys("python")
+        elem.send_keys(Keys.RETURN)
+        assert "www.python.org" in self.driver.page_source
+        time.sleep(1)
+
+    @classmethod
+    def tearDown(self):
+        self.driver.quit()
+
+
+if __name__ == "__main__":
+    # 'html testRunner' package code execution
+    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='D:/Programming/QA/Projects/qa-automation/qa-py-selenium-simple-usage/reports'))
